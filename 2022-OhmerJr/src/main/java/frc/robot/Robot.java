493be7e6be202fe94ba9678b11c148f25226af34
JimHofm52 <JimHofmann52@gmail.com>
2022-01-30 19:49:52 -0500

    Clean up comments.

--- 2022-OhmerJr/src/main/java/frc/robot/Robot.java
+++ 2022-OhmerJr/src/main/java/frc/robot/Robot.java
@@ -14,70 +14,46 @@ import frc.robot.subsystem.drive.Drv_Teleop;
 import frc.robot.subsystem.drive.Trajectories;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the
- * name of this class or
+ * The VM is configured to automatically run this class, 
+ * and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation.
+ * If you change the name of this class or
  * the package after creating this project, you must also update the
- * build.gradle file in the
- * project.
+ * build.gradle file in the project.
  */
 public class Robot extends TimedRobot {
     /**
      * This function is run when the robot is first started up and should be used
-     * for any
-     * initialization code.
+     * for any initialization code.
      */
     @Override
     public void robotInit() {
         IO.init();
         JS_IO.init();
 
-        // Shooter.chsrInit(); //Shooter preset RPMs Chooser
-        Drv_Teleop.chsrInit(); // Drv_Teleop init JS Chooser.
-        Trajectories.chsrInit(); // Drv_Auto init Traj Chooser.
+        Drv_Teleop.chsrInit();      // Init drive type Chooser, Off/Tank/Arcade/Curve.
+        Trajectories.chsrInit();    // Drv_Auto init Traj Chooser.
 
     }
 
     /**
-     * This function is called every robot packet, no matter the mode. Use this for
-     * items like
-     * diagnostics that you want ran during disabled, autonomous, teleoperated and
-     * test.
-     *
+     * This function is called every robot packet, no matter the mode.
+     * Use this for items like diagnostics that you want ran during
+     * disabled, autonomous, teleoperated and test.
      * <p>
      * This runs after the mode specific periodic functions, but before LiveWindow
-     * and
-     * SmartDashboard integrated updating.
+     * and SmartDashboard integrated updating.
      */
     @Override
     public void robotPeriodic() {
-        // Pneu. system has leak. Dont need it when testing drive.
-        // cmprEna = SmartDashboard.getBoolean("Robot/Cmpr Enabled", cmprEna);
-        // IO.compressorRelay.set(IO.compressor.enabled() && cmprEna ? Relay.Value.kForward : Relay.Value.kOff);
+        //No pneumatic system.
         IO.update();
         JS_IO.update();
 
         Trajectories.chsrUpdate();
     }
 
-    /**
-     * This autonomous (along with the chooser code above) shows how to select
-     * between different
-     * autonomous modes using the dashboard. The sendable chooser code works with
-     * the Java
-     * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
-     * chooser code and
-     * uncomment the getString line to get the auto name from the text box below the
-     * Gyro
-     *
-     * <p>
-     * You can add additional auto modes by adding additional comparisons to the
-     * switch structure
-     * below with additional strings. If using the SendableChooser make sure to add
-     * them to the
-     * chooser code above as well.
-     */
+    /** This function is called once when autonomoous is enabled. */
     @Override
     public void autonomousInit() {
         Drv_Auto.init();
